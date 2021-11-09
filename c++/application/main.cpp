ce88b186b69313f68a63a9b4791283ccb22a34c0
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2021-11-10 03:31:54 +0530

    removed build

--- c++/application/main.cpp
+++ c++/application/main.cpp
@@ -10,11 +10,6 @@ int main(int argc, char *argv[])
     std::cin >> text >> pattern;
     ALGORITHMS::KMP::KMP kmp(text, pattern);
     kmp.search();
-    std::cout << "KMP Iterations: " << kmp.getIterations() << std::endl;
-    std::cout << "Found Results at: ";
-    for (auto index : kmp.getSearchIndexes()) {
-        std::cout << index << " ";
-    }
     std::cout << std::endl;
     return 0;
 }
